5db3d9fb2f057e76012a7e371f054f54bf8f0693
HuanDreesL <163208912+HuanDreesL@users.noreply.github.com>
2024-04-04 11:36:23 -0300

    QuantidadeFor.dart

---
+++ QuantidadeFor.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+void main(){
+
+  List<int> inteiros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  
+  for(int i = inteiros.first; i >= 1; i--){
+      stdout.write(inteiros.length);
+  }
+}
