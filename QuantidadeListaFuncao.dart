568ee6f24c147707885eb5078bff09068aa6cbe9
HuanDreesL <163208912+HuanDreesL@users.noreply.github.com>
2024-04-04 11:36:43 -0300

    QuantidadeListaFuncao.dart

---
+++ QuantidadeListaFuncao.dart
@@ -0,0 +1,8 @@
+//Escreva um programa que conte quantas vezes um determinado número aparece em uma lista.
+void main() {
+  List<int> inteiros = [6,5,4,3,2,1];
+  print(inteiros.length);
+  }
+  
+
+
